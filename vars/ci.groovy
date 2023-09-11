304e370fc07000c3b7b10250e21365ffb2df9281
sushma-b23 <susmaboothu5@gmail.com>
2023-09-11 15:28:09 +0530

    if.sh

---
+++ vars/ci.groovy
@@ -0,0 +1,31 @@
+def call() {
+
+    pipeline {
+
+        agent {
+            label 'workstation'
+        }
+        stages {
+            stage('Compile/Build') {
+                steps {
+                    echo 'compile'
+                }
+            }
+            stage('Unit Tests') {
+                steps {
+                    echo 'Unit Tests'
+                }
+            }
+            stage('quality Control') {
+                steps {
+                    echo 'Quality Control'
+                }
+            }
+            stage('Upload Code to Centralized Place') {
+                steps {
+                    echo 'Upload'
+                }
+            }
+        }
+    }
+}
